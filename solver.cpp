c051cc546d1864eea6ac004275255cc5195a87d2
Dongmin Han <dongminhan@gatech.edu>
2019-03-06 21:33:51 -0500

    finish master
    have not tested.

--- solver.cpp
+++ solver.cpp
@@ -64,6 +64,11 @@ bool next_partial_solution(std::vector<unsigned int> &partial_solution,
 	return false;
 }
 
+/* Check if all workers are idle */
+bool check_workers_idle(const bool[] &flags, const int &n) {
+	return std::all_of(flags, flags + n, [](bool working) { return working == false; })
+}
+
 /*************************** solver.h functions ************************/
 void seq_solver(unsigned int n, std::vector<std::vector<unsigned int>> &all_solns) {
 
@@ -85,57 +90,40 @@ void nqueen_master(unsigned int n,
 				   unsigned int k,
 				   std::vector<std::vector<unsigned int> >& all_solns) {
 
+	/* Handle edge case of n = k */
+	if (n == k) {
+		seq_solver(n, all_solns);
+		return;
+	}
 
-
-
-	// TODO: Implement this function
-
-	/* Following is a general high level layout that you can follow
-	 (you are not obligated to design your solution in this manner.
-	  This is provided just for your ease). */
-
-
-	/******************* STEP 1: Send one partial solution to each worker ********************/
-	/*
-	 * for (all workers) {
-	 * 		- create a partial solution.
-	 * 		- send that partial solution to a worker
-	 * }
-	 */
-
-
-	/******************* STEP 2: Send partial solutions to workers as they respond ********************/
-	/*
-	 * while() {
-	 * 		- receive completed work from a worker processor.
-	 * 		- create a partial solution
-	 * 		- send that partial solution to the worker that responded
-	 * 		- Break when no more partial solutions exist and all workers have responded with jobs handed to them
-	 * }
-	 */
-
-	/********************** STEP 3: Terminate **************************
-	 *
-	 * Send a termination/kill signal to all workers.
-	 *
-	 */
-
-	int num_procs;
+	int num_procs; // number of processors
 	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 	MPI_Status stat;
 	MPI_Request req;
+	int worker;
+	int num_workers = num_procs - 1; // number of workers
 
 	/* Create current result */
 	std::vector<unsigned int> curr_res (n, 0);
+
+	/* Next partial solution */
 	std::vector<unsigned int> partial_solution (k, 0);
+
+	/* Sending buffer*/
 	std::vector<unsigned int> res_send (n, 0);
+	/* Receiving buffer */
 	std::vector<unsigned int> res_recv (n, 0);
-	bool had_sol = false, found_sol = false, found_all_sol = false, all_work_done = false;
-	int src;
+
+	/* Flags */
+	bool had_sol = false, found_sol = false, found_all_partial_sol = false, all_work_done = false;
+	
+
+	/* Working flags for workers, false -> idle, true -> working */
+	bool[num_workers] isWorking = {false};
 
 	/******************* STEP 1: Send one partial solution to each worker ********************/
 	/* Find next partial solution for each worker, send them to the workers */
-	for (int w = 0; w < num_procs; w++) {
+	for (int w = 1; w < num_procs; w++) {
 		/* Find next partial solution */
 		found_sol = next_partial_solution(partial_solution, curr_res, 0, n, k, had_sol);
 
@@ -149,13 +137,36 @@ void nqueen_master(unsigned int n,
 	}
 
 	/******************* STEP 2: Send partial solutions to workers as they respond ********************/
-	while (!found_all_sol && !all_work_done) {
+	while (!found_all_partial_sol && !all_work_done) {
+		/* Unblocking receive data from workers */
 		MPI_Irecv(&res_recv[0], n, MPI_INT, MPI_ANY_SOURCE, 101, MPI_COMM_WORLD, &req);
-		found_sol = next_partial_solution(partial_solution, curr_res, 0, n, k, had_sol);
+		
+		/* Find next partial solution */
+		if (!found_all_partial_sol) {
+			found_sol = next_partial_solution(partial_solution, curr_res, 0, n, k, had_sol);
+			found_all_partial_sol = !found_sol;
+		}
+
+		/* Wait for next available worker */
 		MPI_Wait(&req, &stat);
-		src = stat.MPI_SOURCE;
-		res_send = partial_solution;
-		MPI_Send(&res_send[0], n, MPI_INT, src, 100, MPI_COMM_WORLD);
+		worker = stat.MPI_SOURCE;
+		isWorking[worker-1] = false;
+
+		/* Send new partial solution if exist. If all partial solutions are found, check if all workers are done. */
+		if (!found_all_partial_sol) {
+			res_send = partial_solution;
+			MPI_Send(&res_send[0], n, MPI_INT, src, 100, MPI_COMM_WORLD);
+			isWorking[worker-1] = true;
+		} else {
+			all_work_done = check_workers_idle(isWorking, num_workers);
+		}
+	}
+
+	/********************** STEP 3: Terminate **************************/
+	/* Send termination signal to all workers */
+	for (int w = 1; w < num_procs; w++) {
+		res_send[k] = 100;
+		MPI_Send(&res_send[0], n, MPI_INT, w, 100, MPI_COMM_WORLD);
 	}
 }
 
